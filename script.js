c17ea6e90d4905f759f19f83c8ec2a426b6abca2
Thaynansp29 <thaynansp29@hotmail.com>
2022-08-17 20:33:28 -0300

    Inicio de projeto ja com a calculadora programada

---
+++ script.js
@@ -0,0 +1,43 @@
+let inputAdults = document.getElementById("adults");
+let inputChild = document.getElementById("child");
+let inputDuration = document.getElementById("duration");
+
+let resultado = document.getElementById("result");
+
+function calculate() {
+  console.log("Calculando...");
+
+  let adults = inputAdults.value;
+  let child = inputChild.value;
+  let duration = inputDuration.value;
+
+  let tam = meatp(duration) * adults + (meatp(duration) / 2) * child;
+  let tab = beerp(duration) * adults;
+  let tad = drinksp(duration) * adults + (drinksp(duration) / 2) * child;
+
+  result.innerHTML = `<p>${tam / 1000} Kg de Carne</p>`;
+  result.innerHTML += `<p>${Math.ceil(tab / 350)} Latas de Cerveja</p>`;
+  result.innerHTML += `<p>${Math.ceil(tad / 2000)} Garras de Refrigerantes</p>`;
+}
+
+function meatp(duration) {
+  if (duration >= 6) {
+    return 650;
+  } else {
+    return 400;
+  }
+}
+function beerp(duration) {
+  if (duration >= 6) {
+    return 2000;
+  } else {
+    return 1200;
+  }
+}
+function drinksp(duration) {
+  if (duration >= 6) {
+    return 1500;
+  } else {
+    return 1000;
+  }
+}
